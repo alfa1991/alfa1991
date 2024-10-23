56e1ac4367144683f9698fa7f00ba2cd1ecf0f19
Ильгиз <ilgizagliullin@gmail.com>
2024-10-24 00:47:57 +0500

    1.08 Stable Flask

--- flask_app/views.py
+++ flask_app/views.py
@@ -3,6 +3,9 @@
 from flask import Flask, request, jsonify
 from models import db, User
 from app import app
+from flask import render_template, Flask
+
+app = Flask(__name__)
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
@@ -19,3 +22,12 @@ def add_user():
 def get_users():
     users = User.query.all()
     return jsonify([{"id": user.id, "username": user.username, "email": user.email} for user in users]), 200
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Страница по умолчанию
+
+@app.route('/users/')
+def users():
+    users = User.query.all()  # Получаем всех пользователей из базы данных
+    return render_template('users.html', users=users)  # Передаем пользователей в шаблон
