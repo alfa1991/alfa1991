b501701c8b899569464f664802c529b193785d1e
Ильгиз <ilgizagliullin@gmail.com>
2024-10-24 01:20:11 +0500

    1.09 Stable Flask register

--- flask_app/views.py
+++ flask_app/views.py
@@ -17,3 +17,16 @@ def register_user():
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
+
+@app.route('/register', methods=['GET', 'POST'])  # Изменено на '/register'
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))  # Перенаправление на главную страницу
+    return render_template('register.html')  # Отображаем страницу регистрации
+
+
